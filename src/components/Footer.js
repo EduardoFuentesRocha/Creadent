a9790abd14933c16c6785493f64df4f83cfff511
Eduardo Fuentes <95595866+EduardoFuentesRocha@users.noreply.github.com>
2023-04-10 14:12:58 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,14 +5,13 @@ export default () => (
   <div>
     <h2 className="taCenter">
       Síguenos{' '}
-      <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
+      <a href="https://instagram.com/thrivegoldcoast/">@creadentmx</a>
     </h2>
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://www.instagram.com/creadentmx/">creadentmx</a>.
+          © Copyright {new Date().getFullYear()} Todos los derechos reservador. 
         </span>
       </div>
     </footer>
